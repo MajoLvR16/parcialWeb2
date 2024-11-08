f42e45858395521e951a588e0c0095a82cc1839a
MajoLvR16 <mariajoselr@ufps.edu.co>
2024-11-08 10:00:12 -0500

    Creacion del proyecto y del repositorio

---
+++ src/main/java/co/edu/ufps/ParcialWeb2Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialWeb2Application.class, args);
+	}
+
+}
